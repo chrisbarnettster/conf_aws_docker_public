fc5d21621f5d1084fdc3dc74de3dbc71d9497056
chrisbarnettster <chrisbarnettster@gmail.com>
2017-03-01 16:12:13 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Some simple settings for AWS EC2 dev machines. 
+#FUTURE think about using ansible, puppet and improved devops strategies
+
+sudo yum update -y
+sudo yum install tmux git -y
+sudo yum install docker -y
+sudo chkconfig docker
+sudo service docker start
+curl -L https://github.com/docker/compose/releases/download/1.11.1/docker-compose-`uname -s`-`uname -m` >  /tmp/docker-compose
+sudo mv /tmp/docker-compose /usr/local/bin/docker-compose
+sudo chmod +x docker-compose
